b9c053371a0a9bb65a63fbab4e4d6962abd25f51
CaptGL23 <71512656+CaptGL23@users.noreply.github.com>
2022-10-14 07:23:19 -0400

    Add files via upload
    Java code examples from Java 1 Unit 1.

---
+++ Unit 1/Assignment/littendanielunit1/littendweek1_chap1_programmingchallenge/MyFirstProgram/src/MyFirstProgram.java
@@ -0,0 +1,17 @@
+/*
+    Daniel J. Litten II
+    Southwestern College - Professional Studies
+    CSCI 421 - Java Programming Principles I
+    Mr. Russell Schoech
+    February 19, 0222
+*/
+
+// This is my first Java Program.
+public class MyFirstProgram
+{
+    public static void main(String[] args)
+
+    {
+        System.out.println("Hello World!");
+    }
+}
